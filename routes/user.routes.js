ff4d05b802f62480ab1ae08283953dc75cd927fd
adriancoll <acoll@aiudo.es>
2022-04-11 17:31:04 +0200

    feat: route validators

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,7 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
+const { crudValidator } = require("../middlewares/crud-validators");
+const Role = require("../models/role");
 
 const {
   userGet,
@@ -7,6 +10,7 @@ const {
   userDelete,
   userPatch,
 } = require("../controllers/user.controller");
+const { isValidRole } = require("../helpers/db-validators");
 
 const router = Router();
 
@@ -14,7 +18,19 @@ router.get("/", userGet);
 
 router.put("/:id", userPut);
 
-router.post("/", userPost);
+router.post(
+  "/",
+  [
+    check("name", "El nombre es obligatorio.").not().isEmpty(),
+    check("password", "La contraseña debe tener más de 6 letras.").isLength({
+      min: 6,
+    }),
+    check("role").custom(isValidRole),
+    check("email", "El correo no es válido.").isEmail(),
+    crudValidator,
+  ],
+  userPost
+);
 
 router.delete("/", userDelete);
 
