6240dee9d6cf2d4516a16302b18064f4017118c1
adriancoll <acoll@aiudo.es>
2022-04-14 20:46:26 +0200

    fix

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -2,7 +2,7 @@ const { OAuth2Client } = require("google-auth-library");
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-async function googleVerify(token) {
+async function googleVerify(token = '') {
   const ticket = await client.verifyIdToken({
     idToken: token,
     audience: process.env.GOOGLE_CLIENT_ID,
@@ -18,8 +18,6 @@ async function googleVerify(token) {
     email,
   };
 }
-
-googleVerify().catch(console.error);
 
 module.exports = {
   googleVerify,
