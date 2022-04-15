bdc1d63fe1fbd00c53e0b2d9f3cb156ff5d7e7aa
adriancoll <acoll@aiudo.es>
2022-04-15 17:06:24 +0200

    feat: index for models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Service_type = require("./services/service-type.model");
+const Server       = require("./server");
+const User         = require("./user.model");
+const Role         = require("./role.model");
+
+module.exports = {
+  Service_type,
+  Server,
+  User,
+  Role,
+};
