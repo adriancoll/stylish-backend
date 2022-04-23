dca4ade1fb2f9555b73d6ac2865e25011fc8f07e
Adrián Coll <acoll@aiudo.es>
2022-04-23 21:30:23 +0200

    feat: appointmnet routes

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -2,10 +2,31 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 const { crudValidator } = require('../middlewares/crud-validators')
 
+const { storeAppointment } = require('../controllers/appointment.controller')
+
 const router = Router()
 
-router.post('/', {
+router.post('/', [
     crudValidator
-}, storeAppointment)
+], storeAppointment)
+
+/*
+router.post('/confirm/:id', {
+    crudValidator
+}, confirmAppointment)
+
+router.post('/get-all', {
+    crudValidator
+}, getAllAppointments)
+
+router.put('/:id', {
+    crudValidator
+}, updateAppointment)
+
+router.delete('/:id', {
+    crudValidator
+}, deeteAppointment)
+*/
+
 
 module.exports = router
