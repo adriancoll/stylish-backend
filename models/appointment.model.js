a59a8a69f74c5bf58aa7ff58e5c89ec9b9479f70
Adrián Coll <acoll@aiudo.es>
2022-04-23 21:30:15 +0200

    feat: appointment modeling in progreess

--- models/appointment.model.js
+++ models/appointment.model.js
@@ -1,8 +1,24 @@
 const { Schema, model } = require('mongoose')
 
 const AppointmentSchema = new Schema({
-    
-    timestamp: true
+  business: {
+    type: Schema.Types.ObjectId,
+    ref: 'Business',
+    unique: true,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    unique: true,
+    required: true,
+  },
+  status: {
+    type: Number,
+    default: '',
+    emun: ['',''],
+  },
+  timestamp: true,
 })
 
 // Modify the model we get when we query any user to abstract some useless data for frontend
