83b6e0e45993575a48eeb2ccaab0e85413c4927a
Adrián Coll <acoll@aiudo.es>
2022-04-26 23:07:00 +0200

    feat: upload business image

--- controllers/business.controller.js
+++ controllers/business.controller.js
@@ -1,57 +1,61 @@
-const { response, request } = require("express");
-const { isObjectIdArray } = require("../helpers/db-validators");
-const debug = require("../utils/debug");
+const { response, request } = require('express')
+const { isObjectIdArray } = require('../helpers/db-validators')
+const debug = require('../utils/debug')
 
-const Business = require("../models/business.model");
-const User = require("../models/user.model");
+const Business = require('../models/business.model')
+const User = require('../models/user.model')
 
-const { error, success } = require("../helpers");
+const { error, success, fileUpload } = require('../helpers')
 
 const getUserBusiness = async (req = request, res = response) => {
-  const query = { user: req.user.id, status: true };
+  const query = { user: req.user.id, status: true }
 
   try {
     const business = await Business.findOne(query)
-      .populate("user", "-password -__v")
-      .populate("service_types", "-user -__v");
+      .populate('user', '-password -__v')
+      .populate('service_types', '-user -__v')
 
     if (!business) {
-      return res.status(401).json(error("No tienes un negocio asociado"));
+      return res
+        .status(401)
+        .json(error('No tienes un negocio asociado', {}, res.statusCode))
     }
 
-    res.json(success("ok", { business }, res.statusCode));
+    res.json(success('ok', { business }, res.statusCode))
   } catch (e) {
     res
       .status(500)
       .json(
         error(
-          "Error inesperado, el usuario no dispone de una empresa.",
+          'Error inesperado, el usuario no dispone de una empresa.',
           {},
           res.statusCode
         )
-      );
+      )
   }
-};
+}
 
 const storeBusiness = async (req = request, res = response) => {
-  const { name, image, user_id, service_types } = req.body;
+  const { name, image, user_id, service_types } = req.body
 
-  const userQuery = { status: true, role: "BUSINESS_ROLE", _id: user_id };
+  const userQuery = { status: true, role: 'BUSINESS_ROLE', _id: user_id }
 
   const [userHasRoleAndExists, exists] = await Promise.all([
     await User.findOne(userQuery),
     Business.findOne({ user: user_id }),
-  ]);
+  ])
 
   if (!userHasRoleAndExists) {
     return res.status(401).json({
       ok: false,
-      msg: "El usuario no tiene un rol de empresa o no existe",
-    });
+      msg: 'El usuario no tiene un rol de empresa o no existe',
+    })
   }
 
   if (exists) {
-    return res.status(401).json(error("El usuario ya tiene un negocio", null ,res.statusCode));
+    return res
+      .status(401)
+      .json(error('El usuario ya tiene un negocio', null, res.statusCode))
   }
 
   let business = await Business.create({
@@ -59,68 +63,90 @@ const storeBusiness = async (req = request, res = response) => {
     image,
     user: user_id,
     service_types,
-  });
+  })
 
-  business = await Business.findById(business._id).populate(
-    "user",
-    "-password"
-  );
+  business = await Business.findById(business._id).populate('user', '-password')
 
   res.json({
     ok: true,
     business,
-  });
-};
+  })
+}
 
-const updateBusiness = async (req, res) => {
+const updateBusiness = async (req = request, res = response) => {
   try {
-    const { id } = req.params;
+    let business
+    const { id } = req.params
 
-    const { service_types, ...data } = req.body;
+    const { files } = req
+    if (files && Object.keys(files).length > 0) {
+      const imagePath = await fileUpload(files)
+      business = await Business.findByIdAndUpdate(
+        id,
+        { image: imagePath },
+        { new: true }
+      )
+
+      return res.json(
+        success(
+          'Se ha añadido la imágen correctamente',
+          {
+            business,
+          },
+          res.statusCode
+        )
+      )
+    }
+
+    const { service_types, ...data } = req.body
 
     /**
      * Clear duplicated values in case of API
      * bad usage of the user, maybe errors in frontend
      * just make a set of unique values
      */
-    const $service_types = [...new Set(service_types)];
+    const $service_types = [...new Set(service_types)]
 
-    const business = await Business.findByIdAndUpdate(
+    business = await Business.findByIdAndUpdate(
       id,
       { ...data, service_types: $service_types },
       {
         new: true,
       }
-    );
+    )
 
     if (business) {
-      return res.json({
-        ok: true,
-        msg: "Negocio actualizado correctamente",
-        business,
-      });
+      return res.json(
+        success(
+          'Negocio actualizado correctamente',
+          {
+            business,
+          },
+          res.statusCode
+        )
+      )
     }
 
-    res.status(500).json({
-      ok: false,
-      msg: "Error al actualizar el negocio",
-    });
+    res
+      .status(500)
+      .json(error('Error al actualizar el negocio', {}, res.statusCode))
   } catch (e) {
-    debug(e, "error");
+    debug(e, 'error')
+    res.json(error('Error al actualizar el negocio', {}, res.statusCode))
   }
-};
+}
 
-const getAllBusiness = async (_req, res) => {
+const getAllBusiness = async (_req = request, res = response) => {
   const businesses = await Business.find()
-    .populate("user", "-passowrd -__v")
-    .populate("service_types", "-user -__v");
+    .populate('user', '-password -status -__v')
+    .populate('service_types', '-user -status -__v')
 
-  res.json(businesses);
-};
+  res.json(success('OK', businesses, res.statusCode))
+}
 
 module.exports = {
   getUserBusiness,
   storeBusiness,
   updateBusiness,
   getAllBusiness,
-};
+}
