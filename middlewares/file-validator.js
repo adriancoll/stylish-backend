7d8974a2267663a9e15b93446c39ecbeb8f60527
adriancoll <acoll@aiudo.es>
2022-04-27 17:54:32 +0200

    feat: file middleware

---
+++ middlewares/file-validator.js
@@ -0,0 +1,15 @@
+const { request, response } = require("express");
+const { error } = require("../helpers/api-response");
+
+const fileValidator = (req = request, res = response, next) => {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
+    res.status(400).json(error("No se ha adjuntado ningún archivo.", res.statusCode));
+    return;
+  }
+
+  next();
+};
+
+module.exports = {
+  fileValidator,
+};
