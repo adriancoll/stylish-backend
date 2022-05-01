d73d5d02cecba6d8408d4396e23d84ea81e7a057
Adrián Coll <acoll@aiudo.es>
2022-05-02 00:41:27 +0200

    fix wtf

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,7 @@
 const file         = require("./file-upload");
 const paginator    = require("./paginator");
-const dbvalidators = require("./paginator");
-const googleverify = require("./paginator");
+const dbvalidators = require("./db-validators");
+const googleverify = require("./google-verify");
 const jwtgenerator = require("./generate-jwt");
 const responses    = require("./api-response");
 
